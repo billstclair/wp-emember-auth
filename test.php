f34085128a43f8f4da5ef93d0aa7dc70d7893782
Bill St. Clair <billstclair@gmail.com>
2016-09-06 11:48:01 +0000

    wpea_integrate_verify_user() verifies login for SMF, and creates member row if it does not yet exist.

--- test.php
+++ test.php
@@ -11,12 +11,28 @@ $wpea_auth_key = 'dummy-auth-key';
 // AUTH_SALT
 $wpea_auth_salt = 'dummy-auth-salt';
 
+// Map WP eMember username to SMF member name
+// Useful for adding WP eMember authentication to an existing SMF forum.
+$wpea_smf_member_names = array('BillSC' => 'Bill St. Clair'
+			       );			       
+
+// Set if you want new members to be put into a group
+$wpea_smf_member_group_id = 9;
+
+// These are set in the SMF Settings.php file. Need them here for testing.
+$db_type = 'mysql';
+$db_server = 'localhost';
+$db_name = 'smf';
+$db_user = 'root';
+$db_passwd = 'root';
+$db_prefix = 'smf_';
+
 require_once('wp-emember-auth.php');
 
 $cookie_name = wpea_auth_cookie_name();
-$user = 'BillSC';
+$user = 'billstclair';  //'BillSC';
 $pass = wpea_get_user_password($user);
-$time = '1474327520';
+$time = '1474369886';  //'1474327520';
 $hmac = wpea_user_password_hmac($user, $time);
 
 // Fake the cookie
@@ -24,7 +40,13 @@ $cookie = "$user|$time|$hmac";
 $_COOKIE[wpea_auth_cookie_name()] = $cookie;
 
 $logged_in_user = wpea_logged_in_username();
+$smf_user_id = wpea_integrate_verify_user();
 
 wpea_close_db();
+wpea_close_smf_db();
 
-echo "logged in user: $logged_in_user\n";
+$member_name = wpea_username_to_smf_member_name($user);
+$other_name = wpea_username_to_smf_member_name('frobulate');
+echo "member_name: $member_name, other_name: $other_name\n";
+
+echo "logged in user: $logged_in_user, smf_user_id: $smf_user_id\n";
--- wp-emember-auth.php
+++ wp-emember-auth.php
@@ -36,43 +36,102 @@ function wpea_close_db() {
   }
 }
 
+$wpea_smf_conn = null;
+
+function wpea_open_smf_db() {
+  global $wpea_smf_conn;
+  // These variables need to be added to your SMF Settings.php file, copied from your WordPress wp-config.php
+  global $db_type, $db_server, $db_name, $db_user, $db_passwd, $db_prefix;
+
+  if ($wpea_smf_conn) return $wpea_smf_conn;
+
+  if (isset($db_type, $db_server, $db_name, $db_user, $db_passwd, $db_prefix)) {
+    if ($db_type != 'mysql') return null;
+    $wpea_smf_conn = mysqli_connect($db_server, $db_user, $db_passwd, $db_name);
+    return $wpea_smf_conn;
+  }
+  return null;
+}
+
+function wpea_close_smf_db() {
+  global $wpea_smf_conn;
+  if ($wpea_smf_conn) {
+    $conn = $wpea_smf_conn;
+    $wpea_smf_conn = null;
+    mysqli_close($conn);
+  }
+}
+
+function wpea_get_conn_value($conn, $tbl, $name_col, $val_col, $name) {
+  if ($stmt = mysqli_prepare($conn, "SELECT $val_col FROM $tbl WHERE $name_col=? LIMIT 1")) {
+    mysqli_stmt_bind_param($stmt, "s", $name);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    $value = null;
+    mysqli_stmt_bind_result($stmt, $value);
+    mysqli_stmt_fetch($stmt);
+    mysqli_stmt_close($stmt);
+    return $value;
+  }
+  return null;
+}
+
 function wpea_get_db_value($tbl, $name_col, $val_col, $name) {
   global $wpea_db_prefix;
   if ($conn = wpea_open_db()) {
     $tbl = $wpea_db_prefix . $tbl;
-    if ($stmt = mysqli_prepare($conn, "SELECT $val_col FROM $tbl WHERE $name_col=? LIMIT 1")) {
-      mysqli_stmt_bind_param($stmt, "s", $name);
-      mysqli_stmt_execute($stmt);
-      mysqli_stmt_store_result($stmt);
-      $value = null;
-      mysqli_stmt_bind_result($stmt, $value);
-      mysqli_stmt_fetch($stmt);
-      mysqli_stmt_close($stmt);
-      return $value;
-    }
+    return wpea_get_conn_value($conn, $tbl, $name_col, $val_col, $name);
   }
   return null;
+}
+
+function wpea_get_smf_db_value($tbl, $name_col, $val_col, $name) {
+  global $db_prefix;
+  if ($conn = wpea_open_smf_db()) {
+    $tbl = $db_prefix . $tbl;
+    return wpea_get_conn_value($conn, $tbl, $name_col, $val_col, $name);
+  }
+  return null;
+}
+
+function wpea_set_conn_value($conn, $tbl, $name_col, $val_col, $name, $value) {
+  if ($stmt = mysqli_prepare($conn, "UPDATE $tbl SET $val_col = ? WHERE $name_col=?")) {
+    mysqli_stmt_bind_param($stmt, "ss", $value, $name);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+  }
 }
 
 function wpea_set_db_value($tbl, $name_col, $val_col, $name, $value) {
   global $wpea_db_prefix;
   if ($conn = wpea_open_db()) {
     $tbl = $wpea_db_prefix . $tbl;
-    if ($stmt = mysqli_prepare($conn, "UPDATE $tbl SET $val_col = ? WHERE $name_col=?")) {
-      mysqli_stmt_bind_param($stmt, "ss", $value, $name);
-      mysqli_stmt_execute($stmt);
-      mysqli_stmt_close($stmt);
-    }
+    wpea_set_conn_value($conn, $tbl, $name_col, $val_col, $name, $value);
   }
-  return null;
+}
+
+function wpea_set_smf__db_value($tbl, $name_col, $val_col, $name, $value) {
+  global $db_prefix;
+  if ($conn = wpea_open_smf_db()) {
+    $tbl = $db_prefix . $tbl;
+    wpea_set_conn_value($conn, $tbl, $name_col, $val_col, $name, $value);
+  }
 }
 
 function wpea_get_option($name) {
   return wpea_get_db_value('options', 'option_name', 'option_value', $name);
 }
 
+function wpea_get_emember_value($username, $column_name) {
+  return wpea_get_db_value('wp_eMember_members_tbl', 'user_name', $column_name, $username);
+}
+
 function wpea_get_user_password($username) {
-  return wpea_get_db_value('wp_eMember_members_tbl', 'user_name', 'password', $username);
+  return wpea_get_emember_value($username, 'password');
+}
+
+function wpea_get_user_email($username) {
+  return wpea_get_emember_value($username, 'email');
 }
 
 function wpea_last_session_impression($hmac) {
@@ -90,16 +149,16 @@ function wpea_wp_auth_salt() {
   return $wpea_auth_key . $wpea_auth_salt;
 }
 
-// From wp-content/plugins/wp-eMember/lib/class.emember_auth.php
-// The b_hash() function
+// The b_hash() function in wp-content/plugins/wp-eMember/lib/class.emember_auth.php
 function wpea_b_hash($data, $scheme = 'auth') {
   $salt = wpea_wp_auth_salt() . 'j4H!B3TA,J4nIn4.';
   return hash_hmac('md5', $data, $salt);
 }
 
-// Inside the validate() function
+// From inside the validate() function in wp-content/plugins/wp-eMember/lib/class.emember_auth.php
 function wpea_user_password_hmac($username, $expiration) {
-  $pass_frag = substr(wpea_get_user_password($username), 8, 4);
+  $password = wpea_get_user_password($username);
+  $pass_frag = substr($password, 8, 4);
   $key = wpea_b_hash($username . $pass_frag . '|' . $expiration);
   return hash_hmac('md5', $username . '|' . $expiration, $key);
 }
@@ -118,7 +177,7 @@ function wpea_cookie_value($cookie_name) {
   return $_COOKIE[$cookie_name];
 }
 
-// Return true if a user's WP eMember subscrition is still in effect.
+// Return true if a user's WP eMember subscription is still in effect.
 // To be done.
 // Need to process member_since & membership_level from wp_eMember_members_tbl,
 // using subscription_period & subscription_unit from wp_eMember_membership_tbl.
@@ -130,7 +189,7 @@ function wpea_is_membership_current($username) {
 // from current_time() in wp-includes/functions.php
 function wpea_current_mysql_time($gmt = 0) {
   $format = 'Y-m-d H:i:s';
-  return $gmt ? gmdate($format) : gmdate($format, time() + (wpea_get_option( 'gmt_offset') * 3600));
+  return $gmt ? gmdate($format) : gmdate($format, time() + (wpea_get_option('gmt_offset') * 3600));
 }
 
 // Finally, the actual authorization function
@@ -156,4 +215,78 @@ function wpea_logged_in_username() {
   $current_time = wpea_current_mysql_time(true);
   wpea_set_last_session_impression($hmac, $current_time);
   return $username;
+}
+
+// Convert a WP eMember username to an SMF member name.
+// This allows configuration to make them different, via $wpea_smf_member_names.
+function wpea_username_to_smf_member_name($username) {
+  global $wpea_smf_member_names;
+  if (isset($wpea_smf_member_names) && is_array($wpea_smf_member_names)) {
+    if (isset($wpea_smf_member_names[$username])) {
+      return $wpea_smf_member_names[$username];
+    }
+  }
+  return $username;
+}
+
+// Returns an SMF member's ID
+function wpea_lookup_smf_member_id($member_name) {
+  return wpea_get_smf_db_value('members', 'member_name', 'id_member', $member_name);
+}
+
+function wpea_create_smf_member($wp_name, $smf_name) {
+  global $db_prefix, $wpea_smf_member_group_id;
+  $email = wpea_get_user_email($wp_name);
+  $group = 0;
+  if (isset($wpea_smf_member_group_id)) {
+    $group = $wpea_smf_member_group_id;
+  }
+  if ($conn = wpea_open_smf_db()) {
+    $date = time();
+    $tbl = $db_prefix . 'members';
+    echo "Inserting '$wp_name' into SMF as '$smf_name', email: $email, date: $date, tbl; $tbl\n";
+    if ($stmt = mysqli_prepare($conn, "INSERT INTO $tbl (member_name, date_registered, id_group, real_name, email_address, hide_email) VALUES (?, ?, ?, ?, ?, 1)")) {
+      mysqli_stmt_bind_param($stmt, "siiss", $smf_name, $date, $group, $smf_name, $email);
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_close($stmt);
+      echo "  succeeded.\n";
+      return true;
+    } else {
+      $err = mysqli_connect_error();
+      echo "Error inserting into $tbl: $err\n";
+    }
+  }
+  return false;
+}
+
+// Checks WP eMember session cookie for validity
+// If valid, creates SMF 'members' table row if it doesn't exist,
+// and returns the SMF id_member.
+// If invalid, or can't find or create SMF member, return 0.
+// This is as expected by http://wiki.simplemachines.org/smf/Integration_hooks#integrate_verify_user
+function wpea_integrate_verify_user() {
+  $username = wpea_logged_in_username();
+  $res = 0;
+  if ($username) {
+    $smf_member_name = wpea_username_to_smf_member_name($username);
+    if ($smf_member_name) {
+      $id = wpea_lookup_smf_member_id($smf_member_name);
+      if (!$id) {
+	if (wpea_create_smf_member($username, $smf_member_name)) {
+	  $id = wpea_lookup_smf_member_id($smf_member_name);
+	}
+      }
+      if ($id) $res = $id;
+    }
+  }
+  wpea_close_smf_db();
+  wpea_close_db();
+  return $res;
+}
+
+// This goes in the mods install.php file. Included here because I'm looking at the doc now.
+// http://wiki.simplemachines.org/smf/Integration_hooks#Adding_to_hooks
+function wpea_add_integration_functions() {
+  add_integration_function('integrate_pre_include', '$sourcedir/wp-emember-auth.php',TRUE);
+  add_integration_function('integrate_verify_user', 'wpea_integrate_verify_user', TRUE);
 }
